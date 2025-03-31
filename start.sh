a03189081c301b35ebed6599d1ba10da0163c989
alex <alex.moyo.ardanaz@gmail.com>
2025-03-31 08:41:57 +0200

    first commit v2

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo systemctl enable mongod
-sudo systemctl start mongod
-python3 -m http.server 8080 &
-node /home/alejandromoyo/work/Control-WAF-rules/backend/server.js &
-
-echo "Servidores iniciados en segundo plano."
-echo "Para detenerlos, busca sus PIDs con 'ps aux | grep <nombre_del_proceso>' y usa 'kill <PID>'."
